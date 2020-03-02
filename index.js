eaa606250ab5eee25c5a72eecae74eb331e59db0
NasirK24 <nasirmkhalid24@gmail.com>
2020-03-02 09:36:05 +0400

    🍎 Iphone fix

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// window.onload = function() {
-//     document.getElementById('stable').addEventListener("markerFound", (e)=>{ alert("Found Horse")});
-//     document.getElementById('live').addEventListener("markerFound", (e)=>{ alert("Found Live")});
-//     document.getElementById('one').addEventListener("markerFound", (e)=>{ alert("Found 1")});
-//     document.getElementById('two').addEventListener("markerFound", (e)=>{ alert("Found 2")});
-//     document.getElementById('three').addEventListener("markerFound", (e)=>{ alert("Found 3")});
-//     document.getElementById('four').addEventListener("markerFound", (e)=>{ alert("Found 4")});
-// }
-
+window.addEventListener('click', function () {
+    var v = document.querySelector('#vid');
+    v.play();
+});
